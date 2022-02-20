6bedc3725109a55301edec73ba4dec78f032225d
Erdem Alpay <erdemalpay@gmail.com>
2022-02-20 23:47:15 +0300

    Use spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useTables } from "../utils/hooks/useTables";
 import { format } from "date-fns";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home: NextPage = () => {
   const date = format(new Date(), "yyyy-MM-dd");
@@ -30,14 +31,20 @@ const Home: NextPage = () => {
 
       <div className="w-4/5 md:w-1/3 h-1/4 absolute m-auto top-0 bottom-0 right-0 left-0">
         <div className="bg-gray-200 p-4 rounded-lg flex flex-col justify-center">
-          {isLoading && <h1 className="text-lg">Kontrol ediliyor</h1>}
+          <ClipLoader loading={isLoading} size={15} />
           {!isLoading && <h1 className="text-lg text-center">{message}</h1>}
-          <div className="flex bg-gray-400 rounded-lg justify-center w-full text-2xl md:hidden">
-            <a href="tel:03128200301">Kafeyi ara</a>
-          </div>
-          <div className="justify-center w-full text-2xl hidden md:flex">
-            <a href="tel:03128200301">Kafe telefon numarasi: 0312 820 03 01</a>
-          </div>
+          {!isLoading && (
+            <>
+              <div className="flex bg-gray-400 rounded-lg justify-center w-full text-2xl md:hidden">
+                <a href="tel:03128200301">Kafeyi ara</a>
+              </div>
+              <div className="justify-center w-full text-2xl hidden md:flex">
+                <a href="tel:03128200301">
+                  Kafe telefon numarasi: 0312 820 03 01
+                </a>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
